f70ec3225b2cbe3a38cb985fd3830f9c55565d92
Matt Koski <maccam912@gmail.com>
2022-06-25 21:32:20 -0500

    start docker

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 # sudo dnf install git curl -y
 # curl -sSL https://get.docker.com | bash
+# systemctl enable --now docker
 docker build -t server .
 docker run -d -p 5672:5672 -p 15672:15672 -p 1883:1883 server
